643205945f3e8fdb5e706ffddcfae2a67ca82706
Dmytro Malkin <malkin.dmytro@gmail.com>
2024-02-26 13:31:25 +0100

    error handle added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from "./ui/Home";
 import Menu, {loader as menuLoader} from "./features/menu/Menu";
 import Cart from "./features/cart/Cart"
 import Order from "./features/order/Order"
 import CreateOrder from "./features/order/CreateOrder"
+import Home from "./ui/Home";
 import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
 
 const router = createBrowserRouter([
     {
         element: <AppLayout/>,
+        errorElement: <Error/>,
         children: [
             {path: "/", element: <Home/>},
             {
                 path: "/menu",
                 element: <Menu/>,
                 loader: menuLoader,
+                errorElement: <Error/>,
             },
             {path: "/cart", element: <Cart/>},
             {path: "/order/:orderId", element: <Order/>},
