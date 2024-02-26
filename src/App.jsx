e02e2c98777c41458d37c32bf389c07e6cf647af
Dmytro Malkin <malkin.dmytro@gmail.com>
2024-02-26 13:54:42 +0100

    order fetch added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Menu, {loader as menuLoader} from "./features/menu/Menu";
 import Cart from "./features/cart/Cart"
-import Order from "./features/order/Order"
+import Order, {loader as orderLoader} from "./features/order/Order"
 import CreateOrder from "./features/order/CreateOrder"
 import Home from "./ui/Home";
 import AppLayout from "./ui/AppLayout.jsx";
@@ -20,7 +20,12 @@ const router = createBrowserRouter([
                 errorElement: <Error/>,
             },
             {path: "/cart", element: <Cart/>},
-            {path: "/order/:orderId", element: <Order/>},
+            {
+                path: "/order/:orderId",
+                element: <Order/>,
+                loader: orderLoader,
+                errorElement: <Error/>,
+            },
             {path: "/order/new", element: <CreateOrder/>},
         ]
     },
