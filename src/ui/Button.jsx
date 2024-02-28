8874fffcff51fabe07968a623d008eac4e7a71d4
Dmytro Malkin <malkin.dmytro@gmail.com>
2024-02-28 19:29:00 +0100

    styling create order

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,13 +6,13 @@ function Button({children, disabled, to, type}) {
         hover:bg-yellow-300 transition-colors duration-300 
         focus:outline-none focus:ring focus:ring-yellow-300 
         focus:bg-yellow-300 focus:ring-offset-2 
-        disabled:cursor-not-allowed`;
+        disabled:cursor-not-allowed text-sm`;
     const
         styles = {
             primary: base + " py-3 px-4 md:px-6 md:py-4",
             small: base + " py-2 px-4 md:px-5 md:py-2.5 text-xs",
             secondary: `uppercase font-semibold border-2 border-stone-300 
-        rounded-full text-stone-400 inline-block tracking-wide 
+        rounded-full text-stone-400 inline-block tracking-wide text-sm 
         hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 
         focus:outline-none focus:ring focus:ring-stone-200 
         focus:bg-stone-300 focus:ring-offset-2 focus:text-stone-800
@@ -28,10 +28,9 @@ function Button({children, disabled, to, type}) {
         );
 
     return (
-        <div disabled={disabled}
-             className={styles[type]}>
+        <button disabled={disabled} className={styles[type]}>
             {children}
-        </div>
+        </button>
     );
 }
 
